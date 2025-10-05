9f51306902534a0c52ffb0815a882df5643386b1
NikolayT-qa <yalokinquiet@gmail.com>
2025-10-05 19:51:29 +0300

    Update test_trener.py

--- PythonProjects/tests/test_trener.py
+++ PythonProjects/tests/test_trener.py
@@ -2,7 +2,7 @@ import requests
 import pytest
 
 URL='https://api.pokemonbattle.ru/v2'
-TOKEN = '3867b6550810443a45aced88cd9fe986'
+TOKEN = ''
 HEADER={'Content-Type':'application/json', 'trainer_token': TOKEN}
 TRAINER_ID = '41448'
 
@@ -13,4 +13,5 @@ def test_status_code():
 
 def test_my_trainer():
     response_my_trainer = requests.get(url=f'{URL}/trainers', params = {'trainer_id' : TRAINER_ID} )
+
     assert response_my_trainer.json()["data"][0]["id"] == TRAINER_ID
